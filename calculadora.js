60f80bb99d64e5ad99aaa979820bd979dcefb746
oscarcalle <oscarcalle0@gmail.com>
2024-05-09 08:18:29 -0500

    funcion-calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+//funcion para calcular
+
+function calcular(num1, num2, op){
+    switch(op){
+        case"+":
+        return sumar (num1, num2);
+        case"-":
+        return restar (num1, num2);
+        case"/":
+        return dividir (num1, num2);
+        case"*":
+        return multiplicar (num1, num2);
+        default:
+            return"Operacion no valida"
+    }
+}
+
+//45
+
+//samurai
+
+//cristina
